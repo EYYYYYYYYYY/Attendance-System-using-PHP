b6796bbed6537bbbae42cf24b3e19ce0b1b9bd08
SHOGUN <jhonhaze09@gmail.com>
2023-08-29 10:33:16 +0800

    Resolved conflicts in index.php

---
+++ Admin/Includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> -Modified By BSIT 2-5 MARIADB
+            
+            </span>
+          </div>
+        </div>
+      </footer>
